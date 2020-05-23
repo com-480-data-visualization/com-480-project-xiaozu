d5124c18795cff2e6382a5031ae5ce0db4cf5fef
Costanza <costanza.volpini@gmail.com>
2020-05-23 19:47:40 +0200

    slide + new style

--- webapp/app/js/main.js
+++ webapp/app/js/main.js
@@ -1,6 +1,12 @@
 $('#menuList a').on('click', function (e) {
-    e.preventDefault()
-    $(this).tab('show')
-  })
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+$('#slide-courses').click(function () { 
+  var $marginLefty = $("#selected-courses");
+  $marginLefty.animate({ 
+    marginLeft: parseInt($marginLefty.css('marginLeft'), 10) == 600 ? 800 : 600 });
+  });
 
 q = d3.queue();
