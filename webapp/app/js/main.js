3d41beee653b1e728c0376bfec8a0b52b6089d49
Costanza <costanza.volpini@gmail.com>
2020-05-24 11:11:54 +0200

    style updated

--- webapp/app/js/main.js
+++ webapp/app/js/main.js
@@ -3,10 +3,19 @@ $('#menuList a').on('click', function (e) {
   $(this).tab('show')
 })
 
-$('#slide-courses').click(function () { 
+$('#slide-courses').click(function (e) {
+  var contentBtn = e.currentTarget;
+  if(contentBtn.innerHTML.includes("Show")){
+    contentBtn.innerHTML = `<i class="fas fa-columns"></i> Hide
+    courses done`;
+  } else {
+    contentBtn.innerHTML = `<i class="fas fa-columns"></i> Show
+    courses done`;
+  }
+
   var $marginLefty = $("#selected-courses");
   $marginLefty.animate({ 
-    marginLeft: parseInt($marginLefty.css('marginLeft'), 10) == 400 ? 800 : 400 });
+    marginLeft: parseInt($marginLefty.css('marginLeft'), 10) == 400 ? 820 : 400 });
   });
 
 q = d3.queue();
