bff4a3198893f632b0201d3c397a8a6be94715ce
Costanza <costanza.volpini@gmail.com>
2020-05-27 00:12:17 +0200

    trying to fix css

--- webapp/app/js/main.js
+++ webapp/app/js/main.js
@@ -5,17 +5,24 @@ $('#menuList a').on('click', function (e) {
 
 $('#slide-courses').click(function (e) {
   var contentBtn = e.currentTarget;
+  var toExpand = false;
   if(contentBtn.innerHTML.includes("Show")){
     contentBtn.innerHTML = `<i class="fas fa-columns"></i> Hide
     courses done`;
+    $("#selected-courses")[0].classList.remove("col-1");
+    $("#selected-courses")[0].classList.add("col-12");
+    toExpand = true;
   } else {
     contentBtn.innerHTML = `<i class="fas fa-columns"></i> Show
     courses done`;
+    $("#selected-courses")[0].classList.add("col-1");
+    $("#selected-courses")[0].classList.remove("col-12");
+    toExpand = false;
   }
 
   var $marginLefty = $("#selected-courses");
   $marginLefty.animate({ 
-    marginLeft: parseInt($marginLefty.css('marginLeft'), 10) == 400 ? 820 : 400 });
+    marginLeft: toExpand ? 400 : 1000 });
   });
 
 q = d3.queue();
