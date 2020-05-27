53a8e2e862287b4979c1a30379e1271db859e5dd
Costanza <costanza.volpini@gmail.com>
2020-05-27 12:55:28 +0200

    fixed "show courses"

--- webapp/app/js/main.js
+++ webapp/app/js/main.js
@@ -9,20 +9,16 @@ $('#slide-courses').click(function (e) {
   if(contentBtn.innerHTML.includes("Show")){
     contentBtn.innerHTML = `<i class="fas fa-columns"></i> Hide
     courses done`;
-    $("#selected-courses")[0].classList.remove("col-1");
-    $("#selected-courses")[0].classList.add("col-12");
     toExpand = true;
   } else {
     contentBtn.innerHTML = `<i class="fas fa-columns"></i> Show
     courses done`;
-    $("#selected-courses")[0].classList.add("col-1");
-    $("#selected-courses")[0].classList.remove("col-12");
     toExpand = false;
   }
 
   var $marginLefty = $("#selected-courses");
   $marginLefty.animate({ 
-    marginLeft: toExpand ? 400 : 1000 });
+    marginLeft: toExpand ? "55%" : "100%" });
   });
 
 q = d3.queue();
