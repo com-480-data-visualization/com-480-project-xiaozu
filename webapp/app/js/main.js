2954790681da2da95b200717a797820ad29c4980
Costanza <costanza.volpini@gmail.com>
2020-05-28 13:25:59 +0200

    tutorial finished

--- webapp/app/js/main.js
+++ webapp/app/js/main.js
@@ -3,8 +3,6 @@ $('#menuList a').on('click', function (e) {
   $(this).tab('show')
 })
 
-
-
 var introguide = introJs();
 
 introguide.onbeforechange(function(targetElement) {
@@ -20,18 +18,20 @@ introguide.onbeforechange(function(targetElement) {
       v += word[n];
       $(".typeahead").typeahead('val', v)
       if (++n < word.length) {
-        setTimeout(loop, 1);
+        setTimeout(loop, 0.2);
       } else {
-        $(".tt-selectable")[0].click()
+        $(".tt-selectable")[0].click();
       }
     })();
 
   }
   else {
-    if(targetElement.id == "lock-demo-8"){
-      var icon = targetElement.children[0].children[0].children[0];
-      icon.click();
-      // small_lock_button_8
+    if(targetElement.id == "slide-courses"){
+      console.log("inside!!!!!!!!!!!!!!!!!!!")
+      $("#course_network")[0].children[1].children[1].children[9].setAttribute("id", "circle-tutorial");
+    }
+    if(targetElement.id == "course_network"){
+      d3.select('#circle-tutorial').dispatch('click'); // click inside d3
     }
     if(targetElement.id != "start-the-tutorial"){
       targetElement.click();
@@ -45,36 +45,36 @@ introguide.setOptions({
   // 'data-disable-interaction': true,
   // 'tooltipPosition': "auto",
   steps: [
-      // {
-      //   element: '.btn-start',
-      //   intro: 'We will guide you around our website for a full immersive experience. <br><br>Use the arrow keys for navigation or hit ESC to exit.',
-      //   position: 'bottom'
-      // },
-      // {
-      //   element: '#explore-btn',
-      //   intro: 'In this tab you can see which are the popular courses over the years.',
-      //   position: 'bottom'
-      // },
-      // {
-      //   element: '#dropdownMenuButton',
-      //   intro: 'Select the sections that you want to show on the bubble graph.',
-      //   position: 'bottom'
-      // },
-      // {
-      //   element: '#checkbox-tutorial',
-      //   intro: 'Check/Uncheck to show/hide a section from the graph.',
-      //   position: 'bottom'
-      // },
-      // {
-      //   element: '#slide-tutorial',
-      //   intro: 'Slide to navigate over different years.',
-      //   position: 'bottom'
-      // },
-      // {
-      //   element: '#statistic-tutorial-bubble',
-      //   intro: 'Here you can see some statistics.',
-      //   position: 'bottom'
-      // },
+      {
+        element: '.btn-start',
+        intro: 'We will guide you around our website for a full immersive experience. <br><br>Use the arrow keys for navigation or hit ESC to exit.',
+        position: 'bottom'
+      },
+      {
+        element: '#explore-btn',
+        intro: 'In this tab you can see which are the popular courses over the years.',
+        position: 'bottom'
+      },
+      {
+        element: '#dropdownMenuButton',
+        intro: 'Select the sections that you want to show on the bubble graph.',
+        position: 'bottom'
+      },
+      {
+        element: '#checkbox-tutorial',
+        intro: 'Check/Uncheck to show/hide a section from the graph.',
+        position: 'bottom'
+      },
+      {
+        element: '#slide-tutorial',
+        intro: 'Slide to navigate over different years.',
+        position: 'bottom'
+      },
+      {
+        element: '#statistic-tutorial-bubble',
+        intro: 'Here you can see some statistics.',
+        position: 'bottom'
+      },
       {
         element: '#network-btn',
         intro: 'In this tab you can explore suggested courses for a specific student.',
@@ -82,7 +82,7 @@ introguide.setOptions({
       },
       {
         element: '#studentNameAutocomplete',
-        intro: 'Insert the student name.',
+        intro: 'Insert the student name. Please wait that the graph is loaded before moving to the next.',
         position: 'bottom'
       },
       {
@@ -107,27 +107,17 @@ introguide.setOptions({
         intro: 'Click here to hide the courses.',
         position: 'bottom'
       },
-      // check how pick 1st element
-      // {
-      //   element: '.circle-tutorial',
-      //   intro: 'Click on a node to see its statistics and (un)lock it.',
-      //   position: 'bottom'
-      // },
-      //
-      // {
-      //   element: '#statistics-network',
-      //   intro: 'Here you can find all the statistic for a course.',
-      //   position: 'bottom'
-      // },
-      // {
-      //   element: '#lock_button',
-      //   intro: 'Click the icon to unlock it. Enjoy our application!',
-      //   position: 'bottom'
-      // }
-
+      {
+        element: '#course_network',
+        intro: 'Click on a node to see its statistics and (un)lock it.',
+        position: 'bottom'
+      },
       
-
-      
+      {
+        element: '#statistics-network',
+        intro: 'Here you can find all the statistic for a course.',
+        position: 'bottom'
+      }
 
      
   ]
